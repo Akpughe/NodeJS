9d50af6a633e5fa5bc7b0a9c4459697142b58d0e
David <davidakpughe2@gmail.com>
2020-02-27 13:03:53 +0100

    error handling - returning error page

--- controllers/error.js
+++ controllers/error.js
@@ -2,3 +2,9 @@ exports.errorPage = (req, res, next) => {
     res.status(404).render('error', { pageTitle: 'Error 404', path: '',
     isAuthenticated: req.isLoggedIn });
   };
+
+
+exports.errorPage500 = (req, res, next) => {
+    res.status(404).render('500', { pageTitle: 'Error 500', path: '/500',
+    isAuthenticated: req.isLoggedIn });
+  };
