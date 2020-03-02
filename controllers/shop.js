be761f1a5e29c578f0e1fa5bde651b8e36001125
David <davidakpughe2@gmail.com>
2020-03-02 11:44:45 +0100

    setting file type headers

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -127,8 +127,8 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: product,
         pageTitle: 'Home Page',
-        path: '/',
-        // isAuthenticated: req.session.isLoggedIn, 
+        path: '/'
+        // isAuthenticated: req.session.isLoggedIn,
         // csrfToken: req.csrfToken()
       });
     })
@@ -137,16 +137,17 @@ exports.getIndex = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  const invoiceName = 'invoice - ' + orderId + '.pdf'
-  const invoicePath = path.join('data', 'Invoices', invoiceName)
-  fs.readFile(invoicePath, (err, data)=> {
-      if(err){
-        return next(err);
-      }
-      res.send(data)
-  })
-
-}
+  const invoiceName = 'invoice - ' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'Invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"')
+    res.send(data);
+  });
+};
 
 // exports.getCheckout = (req, res, next) => {
 //   res.render('shop/checkout', {
